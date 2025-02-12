9bbf36235bbd35109454dfb5d06aefbc2abfa916
MattyPersil <ananaze44@gmail.com>
2025-02-12 23:53:51 +0100

    Created basic task (test)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use serenity::prelude::*;
+use serenity::model::prelude::*;
+use serenity::Client;
+use std::env;
+
+struct Handler;
+
+#[serenity::async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "Bonjour" {
+            if let Err(e) = msg.channel_id.say(&ctx.http, "Salut !").await {
+                eprintln!("Erreur en envoyant le message : {:?}", e);
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let token = "MTMzOTIxOTA4OTMyMjY3NjMxOA.GF37qP.sF6aAPyYMJdEU_wII19MpfAACW6rC6JmqpvwjM";
+    
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+    let mut client = Client::builder(token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("Erreur lors de la création du client");
+
+    if let Err(e) = client.start().await {
+        eprintln!("Erreur du bot : {:?}", e);
+    }
 }
